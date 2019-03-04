38b908c1dca07d06f9fc0df0a42f865546e89c53
Vinícius Silvano <viniciusssf@hotmail.com>
2019-03-04 19:32:14 -0300

    modificando urls novamente para producao

--- src/app/projetos-classes/TECNOLOGIAS.ts
+++ src/app/projetos-classes/TECNOLOGIAS.ts
@@ -4,13 +4,13 @@ export const TECNOLOGIAS: Tecnologia[] = [
     {
         idTecnologia:1,
         nome:"ANDROID",
-        imagemLocation:"../..portifolioVinicius/assets/images/android-logo.png",
+        imagemLocation:"../../portifolioVinicius/assets/images/android-logo.png",
         idEspecificacao:1,
     },
     {
         idTecnologia:2,
         nome:"SPRING BOOT",
-        imagemLocation:"../..portifolioVinicius/assets/images/spring-boot-logo.jpg",
+        imagemLocation:"../../portifolioVinicius/assets/images/spring-boot-logo.jpg",
         idEspecificacao:2
     }
 ]
