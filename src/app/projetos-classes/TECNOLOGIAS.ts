f656b0c2ca11df2c424c4814dc48d9d155ac398f
Vinícius Silvano <viniciusssf@hotmail.com>
2019-08-24 20:46:28 -0300

    adicionando classe abstrata de Strings globais e adicionando a url da produção e desenvolvimento junto com a classe

--- src/app/projetos-classes/TECNOLOGIAS.ts
+++ src/app/projetos-classes/TECNOLOGIAS.ts
@@ -1,17 +1,18 @@
-import {Tecnologia, TecnologiaAuxiliar} from "./tecnologia";
+import {Tecnologia} from "./tecnologia";
+import { GlobalStrings } from '../globalStrings/globalString';
 
 export const TECNOLOGIAS: Tecnologia[] = [
     {
         idTecnologia:1,
         nome:"ANDROID",
-        imagemLocation:"../../assets/images/android-logo.png",
+        imagemLocation:"../.." + GlobalStrings.getGlobalUrlFragment()+ "/assets/images/android-logo.png",
         idEspecificacao:1,
     },
     {
         idTecnologia:2,
         nome:"SPRING BOOT",
        // imagemLocation:"../../portifolioVinicius/assets/images/spring-boot-logo.jpg",
-        imagemLocation:"../../assets/images/spring-boot-logo.jpg",
+        imagemLocation:"../.." + GlobalStrings.getGlobalUrlFragment() + "/assets/images/spring-boot-logo.jpg",
         idEspecificacao:2
     }
 ]
