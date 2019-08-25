e1cde406b75d9cf6058c0b52fca97d0dd73ab685
Vinícius Silvano <viniciusssf@hotmail.com>
2019-08-24 21:58:00 -0300

    arrumando imagens que não apareciam em desenvolvimento devido a url errada, por meio das GlobalString

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalStrings } from '../globalStrings/globalString';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  globalUrl : String = GlobalStrings.getGlobalUrlFragment();
   constructor() { }
 
   ngOnInit() {
