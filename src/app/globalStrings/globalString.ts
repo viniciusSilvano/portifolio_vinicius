25be3974adbe56e21cf80c9374bc1447ce31d359
Vinicius Felipe <viniciusssf3@gmail.com>
2022-10-23 15:43:45 -0300

    Mudando nome do projeto.

--- src/app/globalStrings/globalString.ts
+++ src/app/globalStrings/globalString.ts
@@ -2,7 +2,7 @@
 
 export abstract class GlobalStrings{
     //
-   private static prodUrl: String = '/portifolioVinicius';
+   private static prodUrl: String = '/portifolio_vinicius';
    private static devUrl: String = '/';
 
    public static getGlobalUrlFragment(): String{
